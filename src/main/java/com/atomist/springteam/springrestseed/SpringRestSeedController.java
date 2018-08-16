42ab42c1530d5f39c8ec93b6faa8489f598167fb
Rod Johnson <rod@atomist.com>
2018-08-16 11:39:35 -0400

    Update SpringRestSeedController.java

--- src/main/java/com/atomist/springteam/springrestseed/SpringRestSeedController.java
+++ src/main/java/com/atomist/springteam/springrestseed/SpringRestSeedController.java
@@ -27,7 +27,7 @@ class SpringRestSeedController {
 
     @RequestMapping(method = GET, path = "/")
     public String root() {
-        return "Spring Boot REST service running in IL " + getClass().getName();
+        return "Spring Boot REST service running in Merchandise Mart " + getClass().getName();
     }
 
     @RequestMapping(method = GET, path = "hello/{name}")
